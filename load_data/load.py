431de39acf041ea3ffb3260d9e2d19e2c0db5622
Mhoang26 <manhhoang2608tt@gmail.com>
2025-05-20 17:59:32 +0700

    update load

--- load_data/load.py
+++ load_data/load.py
@@ -11,11 +11,17 @@ def load_data(input_folder, output_data):
     collection_name = os.path.basename(os.path.normpath(input_folder))
     print(collection_name)
     collection = db[collection_name]
+    # Đường dẫn tới file đã xử lý
+    loaded_path="module_etl_project/load_data/loaded_files.txt"
+    # Đọc danh sách file đã xử lý
+    with open(loaded_path, "r") as f:
+        loaded_files = f.read().splitlines()
+
+
     # Duyệt qua tất cả file CSV trong output_data
     for file in os.listdir(output_data):
-        if file.endswith(".csv"):
+        if file.endswith(".csv") and file not in loaded_files:
             file_path = os.path.join(output_data, file)
-
             try:
                 df = pd.read_csv(file_path)
 
@@ -26,5 +32,8 @@ def load_data(input_folder, output_data):
 
                 if records:
                     collection.insert_many(records)
+                with open(loaded_path, "a") as f:
+                    f.write(file + "\n")
+                print("đã load")
             except Exception as e:
                 print(f" Lỗi khi xử lý {file}: {e}")
